ae94687682e506ab0ce66efc9b05a9be60b2fe13
Marcio Ferreira <marciosferreira@yahoo.com.br>
2022-10-29 19:59:15 +0100

    end

--- wave.py
+++ wave.py
@@ -1,17 +1,15 @@
 
 
 # Check if camera opened successfully
-import cv2
-import seaborn as sns
-import matplotlib.pyplot as plt 
-import pandas as pd
 import pickle
 
-import matplotlib.pyplot as plt
+import cv2
+from matplotlib import pyplot as plt
 import numpy as np
-from scipy.interpolate import interp1d
+import pandas as pd
+import seaborn as sns
 from scipy import stats
-
+from scipy.interpolate import interp1d
 
 with open('C:/Users/marcio/Documents/results_Ian/dict.txt', 'rb') as handle:
   frames_numbers = pickle.loads(handle.read())
@@ -34,6 +32,7 @@ if (cap.isOpened()== False):
   print("Error opening video stream or file")
 
 from itertools import cycle
+
 color_cycle = cycle(((0,0,255),(0,255,0),(255,0,0)))
 
 #frame_n = 0
@@ -161,29 +160,23 @@ for idx_frame in range(0,final_frame,1):
             
          
             
-            xnew = np.linspace(x_norm_2[0], x_norm_2[-1])
-            
-            
-            #model4 = np.poly1d(np.polyfit(x_norm_2, y_norm_2, 2))
-            #plt.plot(xnew, model4(xnew))
-            
-            # Define interpolators.
-            #f_linear = interp1d(x_norm, y_norm)
-            f_cubic = interp1d(x_norm_2, y_norm_2, kind='quadratic')
+            #xnew = np.linspace(x_norm_2[0], x_norm_2[-1])           
+     
+            #f_cubic = interp1d(x_norm_2, y_norm_2, kind='quadratic')
           
-            plt.plot(x_norm_2, y_norm_2, 'o', label='data')
+            #plt.plot(x_norm_2, y_norm_2, 'o', label='data')
             #plt.draw()
             #plt.plot(x_norm_2, y_norm_2, 'o', label='data')
-            plt.draw()
+            #plt.draw()
             #plt.plot(xnew, f_linear(xnew), '-', label='linear')
             #plt.xlim(0.1925, 0.21)
-            plt.ylim(1.02, 0.98)
-            plt.plot(xnew, f_cubic(xnew), label='quadratic')
+            #plt.ylim(1.02, 0.98)
+            #plt.plot(xnew, f_cubic(xnew), label='quadratic')
             #plt.legend(loc='best')
             
             #plt.draw()
             #plt.pause(0.0)
-            plt.clf()
+            #plt.clf()
             pass
      
           #plt.show()
@@ -246,8 +239,8 @@ summed_distances = df["distances"].mean()/4
 
 def coord_calc(dataf):
   tail_points = dataf["angle_corr_tail"]
-  frame = dataf["index"]
-  print(frame)
+  #frame = dataf["index"]
+  #print(frame)
   for idx, _ in enumerate(tail_points):
     if idx > 0:
       distance = math.hypot(tail_points[idx][0] - tail_points[idx-1][0], tail_points[idx][1] - tail_points[idx-1][1])
@@ -269,24 +262,88 @@ def coord_calc(dataf):
 
   
 
-df = df.reset_index()
+#df = df.reset_index()
 
-print("the reset")
-print(df)
+
 
 df.apply(coord_calc, axis=1) 
 
 
 
-for row in df.iterrows():
+print(df)
+pass
 
-  
-  x_list = list(zip(*row[1][5]))[0]
-  y_list = list(zip(*row[1][5]))[1]
-  
-  plt.ylim(1.02, 0.98)
-  plt.xlim(1, 1.08)
-  #plt.plot(xnew, f_cubic(xnew), label='quadratic')
-  plt.plot(x_list, y_list, 'o', label='data')
-  plt.pause(0.5)
-  plt.clf()
+
+
+# Read until video is completed
+for idx_frame in range(0,final_frame,1):  
+  if idx_frame in frames_numbers:  
+    print(idx_frame)   
+    cap.set(1, idx_frame)    
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:      
+          if previous_id != frames_numbers[idx_frame][2]:
+            color = next(color_cycle)
+          cv2.rectangle(frame, (frames_numbers[idx_frame][0]-30,frames_numbers[idx_frame][1]-30), (frames_numbers[idx_frame][0]+30, frames_numbers[idx_frame][1]+30), color, 2)
+          font = cv2.FONT_HERSHEY_SIMPLEX
+          cv2.putText(frame, str(frames_numbers[idx_frame][2]),(frames_numbers[idx_frame][0]-30,frames_numbers[idx_frame][1]-30), font, 0.5, (0,0,0), 1) 
+          previous_id = frames_numbers[idx_frame][2]          
+          for coords in frames_numbers[idx_frame][-1]:         
+            cv2.circle(frame, (coords[1], coords[0]), 2, (0, 0, 255), -1)           
+          cv2.imshow('Frame', frame)
+          
+          tail_coordinates = df.loc[idx_frame]['angle_corr_tail']
+          x_list = list(zip(*tail_coordinates))[0]
+          y_list = list(zip(*tail_coordinates))[1]
+          
+          plt.ylim(1.02, 0.98)
+          plt.xlim(1, 1.08)
+          
+          xnew = np.linspace(x_list[0], x_list[-1])           
+            
+          #f_cubic = interp1d(x_list, y_list, kind='quadratic')
+          #plt.plot(xnew, f_cubic(xnew), label='quadratic')
+          plt.plot(x_list, y_list, 'o', label='data')
+          
+          model4 = np.poly1d(np.polyfit(x_list, y_list, 3))
+          plt.plot(xnew, model4(xnew))           
+            
+          plt.pause(0.1)
+          #plt.clf()
+          #plt.clf()
+
+          
+          
+          
+          
+          
+          
+          
+          
+                     
+      # Press Q on keyboard to exit
+          if cv2.waitKey(25) & 0xFF == ord('q'):
+    
+              break
+    else:
+      print("no frames 1")
+      break
+      
+      
+# Break the loop if no frame is retrieved
+  else:
+    #print("no frames 2")
+    continue
+#cv2.waitKey(0)  
+cv2.destroyAllWindows() 
+
+
+
+
+
+
+
+
+
+
