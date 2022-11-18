e6804598788a06523218c2b1bb55485b1d8fba56
Marcio Ferreira <marciosferreira@yahoo.com.br>
2022-11-18 19:38:04 +0000

    test four

--- wave.py
+++ wave.py
@@ -52,8 +52,8 @@ color_cycle = cycle(((0,0,255),(0,255,0),(255,0,0)))
 #frame_n = 0
 
 
-for quadrant in [0,1,2,3]: # [0,1,2,3]
-  for fish_ident in [1,2]: # [1,2]    
+for quadrant in [1]: # [0,1,2,3]
+  for fish_ident in [1]: # [1,2]    
     frames_numbers = df[(df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident)].index.values   
     print(frames_numbers)
     previous_id = 0
@@ -225,8 +225,8 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
     
 
     def distances(t):
-      #x_tuple = tuple(zip(*x))[0]
-      #y_tuple = tuple(zip(*x))[1]
+      x_tuple = tuple(zip(*t))[0]
+      y_tuple = tuple(zip(*t))[1]
       distances = []
       for idx, _ in enumerate(t):
         if idx > 0:
@@ -241,34 +241,28 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
         return ast.literal_eval(list_)
     df['angle_corr_tail'] = df['angle_corr_tail'].apply(literal_set)
 
-
     df.loc[(df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident), 'distances'] = df.loc[(df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident), 'angle_corr_tail'].apply(distances)
     #df["distances"] = df["angle_corr_tail"].apply(distances)
 
     #df["max_row_distance"] = np.NaN
 
     standart_distance = df.loc[(df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident), 'distances'].max()
-    #summed_distances = df["distances"].mean()/4
+
 
     def coord_calc(row):
-      tail_points = row["angle_corr_tail"] 
-     
+      tail_points = row["angle_corr_tail"]
+      #print(df.loc[(df.index == row.name) & (df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident), 'angle_corr_tail'])     
       summed_distances = []
       for idx, _ in enumerate(tail_points):
         if idx > 0:
           distance = math.hypot(tail_points[idx][0] - tail_points[idx-1][0], tail_points[idx][1] - tail_points[idx-1][1])          
           summed_distances.append(distance)
-      summed_distances = sum(summed_distances) 
-      print(summed_distances)
-      print(standart_distance)
-         
+      summed_distances = sum(summed_distances)         
       if summed_distances > standart_distance:
-        #print(tail_points)
         while summed_distances > standart_distance:
           #slope, intercept, r, p, std_err = stats.linregress([tail_points[idx][0], tail_points[idx-1][0]], [tail_points[idx][1], tail_points[idx-1][1]])
           for i in range(1, 5):
             tail_points[i][0] = ((tail_points[i][0] - 1)*0.99) + 1
-            print("r1")        
           summed_distances = []
           for idx, _ in enumerate(tail_points):
             if idx > 0:
@@ -276,7 +270,6 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
               summed_distances.append(distance)
           summed_distances = sum(summed_distances) 
       else:
-        print(tail_points)
         while summed_distances < standart_distance:
           for i in range(1, 5):
             tail_points[i][0] = ((tail_points[i][0] - 1)*1.01) + 1          
@@ -286,12 +279,14 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
               distance = math.hypot(tail_points[idx][0] - tail_points[idx-1][0], tail_points[idx][1] - tail_points[idx-1][1])          
               summed_distances.append(distance)
           summed_distances = sum(summed_distances)
-      
-      print(tail_points) 
+    
+      for i in range(0, len(tail_points)):
+        tail_points[i][0] = tail_points[i][0]/summed_distances
+        tail_points[i][1] = tail_points[i][1]/summed_distances     
+     
+  
 
       
-
-
 
 
     df.loc[(df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident)].apply(coord_calc, axis=1) 
@@ -311,7 +306,7 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
     # Read until video is completed
     #sequences = df["sequence"].unique()
     for sequence in sequences:
-      #sequence = 110
+      #sequence = 942
       
       sub_seq = df.loc[df["sequence"]== sequence]
       for idx_frame in sub_seq.index: 
@@ -336,7 +331,7 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
             cv2.rectangle(frame, (the_row["center_of_mass"].iloc[0][0]-30,the_row["center_of_mass"].iloc[0][1]-30), (the_row["center_of_mass"].iloc[0][0]+30, the_row["center_of_mass"].iloc[0][1]+30), color, 2)
             font = cv2.FONT_HERSHEY_SIMPLEX
             #cv2.putText(frame, str(the_row["fish_head"]iloc[0][0], the_row["fish_head"].iloc[0][0]-30), (the_row["fish_head"].iloc[0][1]-30, the_row["fish_head"].iloc[0][1]-30), font, 0.5, (0,0,0), 1) 
-            cv2.putText(frame, str(the_row["sequence"].iloc[0]) + " " + str(the_row["tail_uniformity"].iloc[0]),(the_row["center_of_mass"].iloc[0][0]-30,the_row["center_of_mass"].iloc[0][1]-30), font, 0.5, (0,0,0), 1)
+            cv2.putText(frame, str(the_row["sequence"].iloc[0]),(the_row["center_of_mass"].iloc[0][0]-30,the_row["center_of_mass"].iloc[0][1]-30), font, 0.5, (0,0,0), 1)
             previous_id = the_row["sequence"].iloc[0]
             
             
@@ -359,42 +354,75 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
             
             
             
-            xnew = np.linspace(x_list[0], x_list[-1])           
+                      
               
             #f_cubic = interp1d(x_list, y_list, kind='quadratic')
             #plt.plot(xnew, f_cubic(xnew), label='quadratic')
             
-            print(the_row["tail_uniformity"])
             #if the_row["good_tail"].iloc[0] == True:
+              
+            
+            
+            
+            #substitue values in coords by the poly fir found
+            
+            
+  
+            x_modulated = [float(i)/x_list[0] for i in x_list]
+            y_modulated = [float(i)/y_list[1] for i in y_list]
+            
+            model4 = np.poly1d(np.polyfit(x_modulated, y_modulated, 3))
+            
+            y_modeled = tuple(model4(x_modulated))
+            x_modeled = tuple(x_modulated)
+            
+            xnew = np.linspace(x_modulated[0], x_modulated[-1]) 
+            
+            
+            
+            
+            final_tails = str(tuple(zip(x_modeled, y_modeled)))
+            
             plt.figure(1)
             #plt.ylim(0.5, 1.5)
             #plt.xlim(1, 2)
             #plt.xlim(1, 1.08)
         
           
-            plt.plot(x_list, y_list, 'o', label='data')          
-            model4 = np.poly1d(np.polyfit(x_list, y_list, 3))
-            plt.plot(x_list, y_list)
+            plt.plot(x_modeled, y_modulated, 'o', label='data')          
+            #model4 = np.poly1d(np.polyfit(x_list, y_list, 3))
+            #plt.plot(x_list, y_list)
             #plt.show()
             #plt.pause(0.3)
-                  
+            #plt.figure(2)
+
+            plt.plot(xnew, model4(xnew))
+            #plt.show()
+            plt.pause(1)  
+            #cv2.waitKey(3)       
             
-            plt.figure(2)
+            
+            #plt.pause(2)
+            #plt.clf()
+                      
+            
+
+           
+            
+            cv2.imshow('Frame', frame)
+            #cv2.waitKey(1)
+            
+              
+            #plt.close(1)
+            
+            
+            #plt.figure(2)
             #plt.ylim(0.5, 1.5)
             #plt.xlim(1, 2)
             #plt.xlim(0.970, 1.08)
-            plt.plot(x_list, y_list, 'o', label='data')          
-            #model4 = np.poly1d(np.polyfit(x_list, y_list, 3))
+            #plt.plot(x_list, y_list, 'o', label='data')        
             
             
-            #substitue values in coords by the poly fir found
-            
-            y_modeled = tuple(model4(x_list))
-            x_modeled = tuple(x_list)
-            y_modulated = [(n*1) for n in y_modeled]
-            x_modulated = [(n*1) for n in x_modeled]
-            
-            final_tails = str(tuple(zip(x_modulated, y_modulated)))
             
             df.loc[(df.index == idx_frame) & (df["quadrant"] == quadrant) & (df["fish_id"] == fish_ident), "tail_coords"] = final_tails
 
@@ -418,25 +446,6 @@ for quadrant in [0,1,2,3]: # [0,1,2,3]
             
             
             
-            
-            plt.plot(xnew, model4(xnew))
-            #plt.show()
-            plt.pause(1)  
-            #cv2.waitKey(3)       
-            
-            
-            #plt.pause(2)
-            #plt.clf()
-                      
-            
-
-           
-            
-            cv2.imshow('Frame', frame)
-            #cv2.waitKey(1)
-            
-              
-            #plt.close(1)
             
             
                 
